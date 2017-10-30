4b661000b01fdebaf067785ee6d4422048399911
Will <willyboy_43@hotmail.com>
2017-10-30 13:51:57 +0000

    Move the run logic to app

---
+++ app/app.go
@@ -0,0 +1,67 @@
+package app
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/aziule/conversation-management/app/bot"
+	"github.com/aziule/conversation-management/app/bot/facebook"
+	"github.com/aziule/conversation-management/app/conversation/mongo"
+	"github.com/aziule/conversation-management/app/nlp/wit"
+	"github.com/go-chi/chi"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/mgo.v2"
+)
+
+// Run starts the server and waits for interactions
+func Run(config *Config) {
+	dbParams := mongo.DbParams{
+		DbHost: config.DbHost,
+		DbName: config.DbName,
+		DbUser: config.DbUser,
+		DbPass: config.DbPass,
+	}
+
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{dbParams.DbHost},
+		Database: dbParams.DbName,
+		Timeout:  2 * time.Second,
+	})
+
+	if err != nil {
+		log.Fatalf("An error occurred when connecting to the db: %s", err)
+	}
+
+	defer session.Close()
+
+	b := facebook.NewBot(
+		&facebook.Config{
+			VerifyToken:     config.FbVerifyToken,
+			ApiVersion:      config.FbApiVersion,
+			PageAccessToken: config.FbPageAccessToken,
+			NlpParser:       wit.NewParser(facebook.DefaultDataTypeMap),
+			ConversationRepository: mongo.NewMongodbRepository(&mongo.Db{
+				Session: session,
+				Params:  dbParams,
+			}),
+		},
+	)
+
+	r := chi.NewRouter()
+
+	// Automatically set the bot's webhooks routes
+	for _, webhook := range b.Webhooks() {
+		log.Debugf("%s %s", string(webhook.Method), webhook.Path)
+
+		switch webhook.Method {
+		case bot.HttpMethodGet:
+			r.Get(webhook.Path, webhook.Handler)
+		case bot.HttpMethodPost:
+			r.Post(webhook.Path, webhook.Handler)
+		}
+	}
+
+	log.Debugf("Listening on port %d", config.ListeningPort)
+	http.ListenAndServe(":"+strconv.Itoa(config.ListeningPort), r)
+}
--- cli/run.go
+++ cli/run.go
@@ -2,18 +2,9 @@ package cli
 
 import (
 	"flag"
-	"net/http"
-	"strconv"
-	"time"
 
 	"github.com/aziule/conversation-management/app"
-	"github.com/aziule/conversation-management/app/bot"
-	"github.com/aziule/conversation-management/app/bot/facebook"
-	"github.com/aziule/conversation-management/app/conversation/mongo"
-	"github.com/aziule/conversation-management/app/nlp/wit"
-	"github.com/go-chi/chi"
 	log "github.com/sirupsen/logrus"
-	"gopkg.in/mgo.v2"
 )
 
 // RunCommand is the command responsible for running our bot using the given configuration.
@@ -38,7 +29,6 @@ func (c *RunCommand) Execute(f *flag.FlagSet) error {
 	config, err := app.LoadConfig(c.configFilePath)
 
 	if err != nil {
-		// @todo: move this to the handler
 		log.Fatalf("An error occurred when loading the config: %s", err)
 	}
 
@@ -46,54 +36,7 @@ func (c *RunCommand) Execute(f *flag.FlagSet) error {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	dbParams := mongo.DbParams{
-		DbHost: config.DbHost,
-		DbName: config.DbName,
-		DbUser: config.DbUser,
-		DbPass: config.DbPass,
-	}
-
-	session, err := mgo.DialWithInfo(&mgo.DialInfo{
-		Addrs:    []string{dbParams.DbHost},
-		Database: dbParams.DbName,
-		Timeout:  2 * time.Second,
-	})
-
-	if err != nil {
-		log.Fatalf("An error occurred when connecting to the db: %s", err)
-	}
-
-	defer session.Close()
-
-	b := facebook.NewBot(
-		&facebook.Config{
-			VerifyToken:     config.FbVerifyToken,
-			ApiVersion:      config.FbApiVersion,
-			PageAccessToken: config.FbPageAccessToken,
-			NlpParser:       wit.NewParser(facebook.DefaultDataTypeMap),
-			ConversationRepository: mongo.NewMongodbRepository(&mongo.Db{
-				Session: session,
-				Params:  dbParams,
-			}),
-		},
-	)
-
-	r := chi.NewRouter()
-
-	// Automatically set the bot's webhooks routes
-	for _, webhook := range b.Webhooks() {
-		log.Debugf("%s %s", string(webhook.Method), webhook.Path)
-
-		switch webhook.Method {
-		case bot.HttpMethodGet:
-			r.Get(webhook.Path, webhook.Handler)
-		case bot.HttpMethodPost:
-			r.Post(webhook.Path, webhook.Handler)
-		}
-	}
-
-	log.Debugf("Listening on port %d", config.ListeningPort)
-	http.ListenAndServe(":"+strconv.Itoa(config.ListeningPort), r)
+	app.Run(config)
 
 	return nil
 }
